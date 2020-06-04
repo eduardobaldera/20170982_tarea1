2807d7997b33d526af9e5343ef912a472d3f1205
Eduardo Baldera <20170982@ce.pucmm.edu.do>
2020-06-03 22:39:22 -0400

    Estructura del proyecto creada

---
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -0,0 +1,16 @@
+package edu.pucmm.eict;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("prueba!\n");
+    }
+}
